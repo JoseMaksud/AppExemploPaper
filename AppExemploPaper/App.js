d468ed1dcab5ddd7e277014dcaa8584fec753238
JoseMaksud <jf9718830@gmail.com>
2024-08-02 22:19:33 -0300

    Testando

--- AppExemploPaper/App.js
+++ AppExemploPaper/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, Appbar, Avatar, Card, Checkbox } from 'react-native-paper';
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Card>
+    <Card.Cover source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYcnusY7qZHApivDI5JUGvnp9_Q_N2tv5RQQ&s' }} />
+    </Card>
+
+
+      {/* 
+      <ActivityIndicator animating={true} color={'crimson'} size={100} />
+      
+      <Avatar.Icon size={100} icon="folder" />
+
       <Button icon="camera" mode='contained' onPress={() => console.log("Pressed")}>
         Press me
-      </Button>
+      </Button> 
+      */}
     </SafeAreaView>
   );
 }
